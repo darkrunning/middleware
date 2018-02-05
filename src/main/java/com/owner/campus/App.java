3cdb28e292d0d4a05f624b9c7b361da88177c3b5
darkrunning <oliverpc@oliverdeMacBook-Air.local>
2018-02-05 11:41:51 +0800

    创建项目

---
+++ src/main/java/com/owner/campus/App.java
@@ -0,0 +1,13 @@
+package com.owner.campus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
